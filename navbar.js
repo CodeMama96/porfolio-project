cdb94274d6553c6e4645182f141c45a66b19bad1
Alicia Weenum <weenum.alicia@gmail.com>
2021-07-01 11:52:04 -0400

    lots n lots of changes

--- navbar.js
+++ navbar.js
@@ -1,8 +1,8 @@
-// function myFunction() {
-//   var x = document.getElementById("myToggle");
-//     if (x.style.display === "none") {
-//       x.style.display = "block";
-//     } else {
-//       x.style.display = "none";
-//     }
-// }
+function changeToggle() {
+  var x = document.getElementById("myToggle");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
